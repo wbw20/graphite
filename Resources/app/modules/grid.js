9944905dde552a08a29ebd4a8aa64738cad4293e
William B. Wettersten <wbw20@case.edu>
2013-12-22 22:16:49 -0800

    code much cleaner

--- Resources/app/modules/grid.js
+++ Resources/app/modules/grid.js
@@ -1,45 +1,38 @@
 define(['text!./grid/grid.html', 'text!./grid/item.html'], function(grid, item) {
-    var grid_el = $(grid),
-        create_item = _.template(item),
+    var create_item = _.template(item),
         items = ['plane', 'train', 'robot', 'spam', 'rocket', 'boat', 'rc turret', 'helicopter'];
 
     function Grid() {
-        this.el = grid_el;
-        var self = this;
-        items.forEach(function(name) {
-            var item_el = $(create_item({
-                name: name
-            }));
-
-            /* Handle menu item highlighting */
-            item_el.on('click', function(event) {
-                alert();
-            });
-
-            self.el.append(item_el);
-        });
+        this.el = el = $(grid);
+        el.append($(_render()));
 
         $(window).on('resize', function(event) {
-            var width = $('#content').width(),
-                unit = $('.grid_item').first().width(),
-                num_columns = Math.floor(width/unit),
-                el = '<tr>';
-
-            for (var i = 0; i < items.length; i++) {
-                if (i % num_columns === 0) {
-                    // new row
-                    el = el + '</tr><tr>';
-                }
-
-                el = el + '<td><div class=\'grid_item animated\'>BLAHBLAH</div></td>';
-            }
-
-            el = el + '</tr>'
-
-            $('#grid').empty().append(el);
+            console.log(el);
+            console.log(_render());
+            el.empty().append(_render());
         });
 
         _.extend(this, Backbone.Events);
+    }
+
+    _render = function() {
+        var width = $('#content').width(),
+            unit = 200,
+            num_columns = Math.floor(width/unit),
+            el = '<tr>';
+
+        for (var i = 0; i < items.length; i++) {
+            if (i % num_columns === 0) {
+                // new row
+                el = el + '</tr><tr>';
+            }
+
+            el = el + '<td><div class=\'grid_item animated\'>BLAHBLAH</div></td>';
+        }
+
+        el = el + '</tr>'
+
+        return el;
     }
 
     return Grid;
